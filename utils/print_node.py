fccf19f47f004bc9e271a41997278a53d3b04cec
navid.rashik <navid.rashik@gmail.com>
2021-01-10 11:07:19 +0600

    updated print node

--- utils/print_node.py
+++ utils/print_node.py
@@ -7,7 +7,7 @@ def print_node(pdf_obj):
     #         "source": "test documentation!", "options": {'fit_to_page': True}, 'printer': 69976050}
 
     data = {"title": "My Test PrintJob", "contentType": "pdf_base64", "content": pdf_obj,
-            "source": "test documentation!", "options": {'fit_to_page': True, "dpi": "300*300"}, 'printer': 69976050}
+            "source": "test documentation!", "options": {'fit_to_page': True, "dpi": "300*300"}, 'printer': 69993996}
 
     response = requests.post('https://api.printnode.com/printjobs',
                              auth=('ToDuOdQbZTW5d9n0TP6JFNRfLScEth4vsg1om5AqmOw', ''), data=data)
